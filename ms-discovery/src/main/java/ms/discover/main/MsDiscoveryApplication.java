75fcb946b9943cd789bddfa2d2a3e7e0398bd900
thaint <thaint@blueseed.tv>
2018-11-29 11:04:04 +0700

    Update Eureka discovery. Downgrade Spring version to 1.5.8

--- ms-discovery/src/main/java/ms/discover/main/MsDiscoveryApplication.java
+++ ms-discovery/src/main/java/ms/discover/main/MsDiscoveryApplication.java
@@ -1,5 +1,13 @@
 package ms.discover.main;
 
-public class MsDiscoveryApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@SpringBootApplication
+public class MsDiscoveryApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(MsDiscoveryApplication.class, args);
+    }
 }
